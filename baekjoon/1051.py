1aa6730981da6cc2ea7ae63bc171199374971da0
jaymoonn <sjc01214@naver.com>
2023-07-30 23:46:53 +0900

    23.07.30 문제풀이

--- baekjoon/1051.py
+++ baekjoon/1051.py
@@ -5,12 +5,12 @@ size = 1
 for _ in range(n):
   arr.append(str(input()))
 
-for i in range(n):  # 0 1 2
-  for j in range(m-1):  # 0 1 2 3
-    for k in range(j+1, m): # 1~4 2~4 3~4 4
-      if arr[i][j] == arr[i][k] == arr[i+k-j][j] == arr[i+k-j][k]:
-        print((k-j+1)**2)
-        # if (j-k+1)**2 > size :
-        #   size = (j-k+1)**2
+for i in range(n-1):
+  for j in range(m-1):
+    for k in range(j+1, m):
+      if i+k-j < n : 
+        if arr[i][j] == arr[i][k] == arr[i+k-j][j] == arr[i+k-j][k]:
+          if (k-j+1)**2 > size :
+            size = (k-j+1)**2
 
-# print(size)
+print(size)
