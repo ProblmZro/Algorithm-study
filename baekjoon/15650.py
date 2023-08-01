746aa9b06a459f51f1ff4489c1aba9d984699640
jaymoonn <sjc01214@naver.com>
2023-08-01 12:36:20 +0900

    23.08.01 문제풀이

--- baekjoon/15650.py
+++ baekjoon/15650.py
@@ -1,5 +1,13 @@
+def dfs(start):
+  if len(arr) == m: # 리스트 길이가 m과 동일해지면 리스트 출력
+    print(' '.join(map(str, arr)))  # [1, 2] -> 1 2
+    return
+  for i in range(start, n+1): # 동일하지 않으면 for문 실행
+    if i not in arr:  # 리스트에 i 없으면
+      arr.append(i) # i 추가
+      dfs(i+1)
+      arr.pop() # 다 차면 리스트 비우기
+
 n, m = map(int, input().split())
 arr = []
-for i in range(n):
-  arr.append(i+1)
-print(arr)
+dfs(1)
---
+++ baekjoon/6603.py
@@ -0,0 +1,14 @@
+def dfs():
+  if a == 6:
+    print(' '.join(map(str, )))
+    return
+
+
+while True:
+  arr = list(map(int, input().split()))
+  k = arr[0]
+  s = arr[1:]
+
+  if k == 0: break
+  dfs()
+
