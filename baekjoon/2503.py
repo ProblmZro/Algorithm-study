f42f34f5a0ee8877e56cd12be733896648b0121f
jaymoonn <sjc01214@naver.com>
2023-08-04 19:42:16 +0900

    23.08.04 문제풀이

--- baekjoon/2503.py
+++ baekjoon/2503.py
@@ -1,12 +1,12 @@
 from itertools import permutations
-nums = list(permutations(['1','2','3','4','5','6','7','8','9'],3))
+nums = set(permutations(['1','2','3','4','5','6','7','8','9'],3))
 
 n = int(input())
-lst = []
+to_remove = set()  # 삭제할 집합
+
 for _ in range(n):
   num, s, b = map(int, input().split())
   num = list(str(num))
-
   for i in nums :
     strick, ball = 0, 0
     for j in range(3) : # 0 1 2
@@ -14,6 +14,10 @@ for _ in range(n):
         strick += 1
       elif num[j] in i :
         ball += 1
-    if s == strick and b == ball :  
-      print(type(i))
-      nums.remove(list(i))
+    if not(s == strick and b == ball) :
+      to_remove.add(i)
+      # nums.remove(i)
+
+nums -= to_remove  # to_remove에 있는 원소들을 nums에서 삭제
+
+print(len(nums))
