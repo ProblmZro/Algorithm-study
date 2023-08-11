3461b323cbe9d834035056680b7be710587f19f9
jaymoonn <sjc01214@naver.com>
2023-08-11 21:53:07 +0900

    7568번 문제풀이

--- baekjoon/7568.py
+++ baekjoon/7568.py
@@ -1,3 +1,16 @@
 n = int(input())
+data = []
+ans = []
 for _ in range(n):
   x, y = map(int, input().split())
+  data.append((x, y))
+
+for i in range(n):
+  rank = 1
+  for j in range(n):
+    if data[i][0] < data[j][0] and data[i][1] < data[j][1]:
+      rank += 1
+  ans.append(rank)
+
+for i in ans:
+  print(i, end=" ")
