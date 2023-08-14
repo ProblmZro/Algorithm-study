9c153dd1653c9cd2a0fa9090a4a53d1ceb59abb1
jaymoonn <sjc01214@naver.com>
2023-08-14 23:18:07 +0900

    1929번 문제풀이

---
+++ baekjoon/1929.py
@@ -0,0 +1,12 @@
+M, N = map(int, input().split())
+
+def isPrime(n) :
+  if n == 1:
+    return False
+  for i in range(2, (n**0.5)+1):
+    if n % i == 0:
+      return False
+  return True
+
+for i in range(M, N+1):
+  if isPrime(i) : print(i)
