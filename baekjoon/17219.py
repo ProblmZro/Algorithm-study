8c2bef3a9f699a5a2eab3b43c6d9e76c8a9533ba
jaymoonn <sjc01214@naver.com>
2023-08-28 20:00:38 +0900

    17219번 문제풀이

---
+++ baekjoon/17219.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+dic = {}
+for _ in range(N) : 
+  site, pwd = input().split()
+  dic[site] = pwd
+
+for _ in range(M):
+  print(dic[input()])
