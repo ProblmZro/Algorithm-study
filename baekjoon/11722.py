370ff6ad86da3d18ad10b42f8a1fc2dd1da57f53
ProblmZro <sjc01214@naver.com>
2024-03-22 18:20:17 +0900

    ~24.03.22

--- baekjoon/11722.py
+++ baekjoon/11722.py
@@ -3,9 +3,8 @@ A = [0] + list(map(int, input().split()))
 dp = [1 for _ in range(N+1)]
 
 for i in range(2, N+1):
-  if A[i-1] < A[i]:
-    dp[i] = dp[i-1] + 1
-  else:
-    dp[i] = dp[i-1] 
+  for j in range(1, i):
+    if A[j] > A[i]:
+      dp[i] = max(dp[j]+1, dp[i])
 
-print(dp[-1])
+print(max(dp))
