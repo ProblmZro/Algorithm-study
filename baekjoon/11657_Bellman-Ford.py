64d16687112c57a69eac094b17b00d3b7ab379e9
ProblmZro <sjc01214@naver.com>
2024-12-05 00:50:54 +0900

    11657 문풀

---
+++ baekjoon/11657_Bellman-Ford.py
@@ -0,0 +1,30 @@
+from sys import stdin
+input = stdin.readline
+INF = int(1e9)
+
+def b_f(start):
+  dist[start] = 0
+  for i in range(N):
+    for cur, next, cost in edges:
+      if dist[next] > dist[cur] + cost:
+        dist[next] = dist[cur] + cost
+        if i == N - 1:
+          return True
+  return False
+
+N, M = map(int, input().split())
+edges = []
+dist = [INF] * (N+1)
+
+for _ in range(M):
+  A, B, C = map(int, input().split())
+  edges.append((A, B, C))
+
+if b_f(1):
+  print(-1)
+else:
+  for i in range(2, N+1):
+    if dist[i] == INF:
+      print(-1)
+    else:
+      print(dist[i])
