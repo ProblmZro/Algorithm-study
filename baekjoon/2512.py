8088ee49494e588a11c8d27f0bd9d9b76e16626c
ProblmZro <sjc01214@naver.com>
2025-07-14 20:00:50 +0900

    2512 문풀

---
+++ baekjoon/2512.py
@@ -0,0 +1,21 @@
+N = int(input())
+province = list(map(int, input().split()))
+M = int(input())
+province.sort()
+start, end = 0, province[-1]
+
+while start <= end:
+  goal = M
+  mid = (start + end) // 2
+  for p in province:
+    if p <= mid:
+      goal -= p
+    else:
+      goal -= mid
+  if goal < 0:
+    end = mid - 1
+  else:
+    res = mid
+    start = mid + 1
+
+print(res)
